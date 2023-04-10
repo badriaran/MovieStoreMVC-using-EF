cb1c4ab15adbc436277f304cf536f09263a6924b
Badri Aran <badriaran10@gmail.com>
2023-04-10 20:40:36 +0545

    Finally fixed the previous bugs on admin role .

--- MovieStoreMVC/Controllers/UserAuthenticationController.cs
+++ MovieStoreMVC/Controllers/UserAuthenticationController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MovieStoreMvc.Models.DTO;
 using MovieStoreMvc.Repositories.Abstract;
 using MovieStoreMVC.Models.DTO;
 
@@ -18,21 +19,21 @@ namespace MovieStoreMvc.Controllers
           I have create a complete tutorial for this, you can check the link in description box
          */
 
-        //public async Task<IActionResult> Register()
-        //{
-        //    var model = new RegistrationModel
-        //    {
-        //        Email = "admin@gmail.com",
-        //        Username = "admin",
-        //        Name = "Ravindra",
-        //        Password = "Admin@123",
-        //        PasswordConfirm = "Admin@123",
-        //        Role = "Admin"
-        //    };
-        //    // if you want to register with user , Change Role="User"
-        //    var result = await authService.RegisterAsync(model);
-        //    return Ok(result.Message);
-        //}
+        public async Task<IActionResult> Register()
+        {
+            var model = new RegistrationModel
+            {
+                Email = "admin@gmail.com",
+                Username = "admin",
+              Name = "Badri Aran",
+               Password = "Admin@123",
+               PasswordConfirm = "Admin@123",
+                Role = "Admin"
+            };
+            // if you want to register with user , Change Role="User"
+            var result = await authService.RegisterAsync(model);
+            return Ok(result.Message);
+        }
 
         public async Task<IActionResult> Login()
         {
